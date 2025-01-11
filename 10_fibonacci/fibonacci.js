c94723aa3fe4069e74f2ccbd79bebf983077fe91
laodeng <93652888+laodengcode@users.noreply.github.com>
2025-01-11 22:02:06 +0100

    js fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    index = Number(index);
+    if (index < 0) {
+        return "OOPS";
+    }
+    if (index == 0) {
+        return 0;
+    } else if (index == 1) {
+        return 1;
+    } else if (index == 2) {
+        return 1;
+    } else {
+        n_2 = 1;
+        n_1 = 1;
+        let n = n_2 + n_1;
+        for (let i = 3; i <= index; i++) {
+            n = n_1 + n_2;
+            n_2 = n_1;
+            n_1 = n;
+        }
+        return n;
+    }
 };
 
 // Do not edit below this line
