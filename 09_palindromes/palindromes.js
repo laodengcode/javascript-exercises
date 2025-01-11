9f9f8b9155fdd496a27ad63b2d55fae302a07f3d
laodeng <93652888+laodengcode@users.noreply.github.com>
2025-01-11 21:42:15 +0100

    js palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const regex = /[,. !]+/gi;
+    let pureChar = text.replaceAll(regex, '').toLowerCase();
+    let mirror = Array.from(pureChar).reverse().join('');
+    return pureChar === mirror;
 };
 
 // Do not edit below this line
