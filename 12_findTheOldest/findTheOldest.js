6f3c9b6961f9d0461cbcdf51a2873fc0495b7960
laodeng <93652888+laodengcode@users.noreply.github.com>
2025-01-11 22:23:16 +0100

    js findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    array.sort((a, b) => {
+        let currentYear = new Date().getFullYear();
+        if (!'yearOfDeath' in a) {
+            a['yearOfDeath'] = currentYear;
+        }
 
+        if (!('yearOfDeath' in b)) {
+            b['yearOfDeath'] = currentYear;
+        }
+
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+
+        return (aAge - bAge) > 0 ? -1 : 1;
+    })
+
+    return array[0];
 };
 
 // Do not edit below this line
