ec011064d69b0fe8dd4071f8899fbc79e00fab26
laodeng <93652888+laodengcode@users.noreply.github.com>
2025-01-11 21:11:33 +0100

    js calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function (a, b) {
+  return a + b;
 
 };
 
-const power = function() {
-	
+const subtract = function (a, b) {
+
+  return a - b;
 };
 
-const factorial = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((total, current) => total + current, 0);
+};
+
+const multiply = function (numbers) {
+  return numbers.reduce((total, current) => total * current, 1);
+};
+
+const power = function (a, b) {
+  return a ** b;
+};
+
+const factorial = function (a) {
+  let result = 1;
+  while (a > 1) {
+    result *= a;
+    a--;
+  }
+
+  return result;
+
 };
 
 // Do not edit below this line
